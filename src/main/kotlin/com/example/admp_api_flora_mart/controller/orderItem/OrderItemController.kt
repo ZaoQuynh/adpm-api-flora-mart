ee6c35b5f7074ad7d64ed7a8fbdee6c01b0fdaef
cunmann <Hoangmanh6889@gmail.com>
2025-03-25 20:11:29 +0700

    Completed Cart API

--- src/main/kotlin/com/example/admp_api_flora_mart/controller/orderItem/OrderItemController.kt
+++ src/main/kotlin/com/example/admp_api_flora_mart/controller/orderItem/OrderItemController.kt
@@ -2,20 +2,18 @@ package com.example.admp_api_flora_mart.controller.orderItem
 
 import com.example.admp_api_flora_mart.controller.orderItem.request.AddToCartRequest
 import com.example.admp_api_flora_mart.service.OrderItemService
-import com.example.admp_api_flora_mart.service.OrderService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/v1/orderItem") // Đổi tên endpoint cho rõ ràng hơn
+@RequestMapping("/api/v1/orderItem")
 class OrderItemController(
     private val orderItemService: OrderItemService,
-    private val orderService: OrderService,
 ){
     @PostMapping
     fun addOrderItem(@RequestBody request: AddToCartRequest): ResponseEntity<Any> {
         return try {
-            val newOrderItem = orderItemService.addToCart(request.orderItemDTO, request.cartDTO)
+            val newOrderItem = orderItemService.addToCart(request)
             ResponseEntity.ok(newOrderItem)
         } catch (ex: Exception){
             ResponseEntity.badRequest().body(mapOf("error" to ex.message))
--- src/main/kotlin/com/example/admp_api_flora_mart/service/OrderItemService.kt
+++ src/main/kotlin/com/example/admp_api_flora_mart/service/OrderItemService.kt
@@ -1,10 +1,11 @@
 package com.example.admp_api_flora_mart.service
+import com.example.admp_api_flora_mart.controller.orderItem.request.AddToCartRequest
 import com.example.admp_api_flora_mart.dto.CartDTO
 import com.example.admp_api_flora_mart.dto.OrderItemDTO
 import com.example.admp_api_flora_mart.entity.OrderItem
 
 interface OrderItemService {
-    fun addToCart(orderItem: OrderItemDTO, cartDTO: CartDTO) : OrderItemDTO
+    fun addToCart(request: AddToCartRequest) : OrderItemDTO
     //fun addItemToCart(customerId: Long, newItem: OrderItem): OrderItemDTO
     fun updateItemQuantity(itemId: Long, qty: Int): OrderItemDTO
     fun removeItemFromCart(itemId: Long)
