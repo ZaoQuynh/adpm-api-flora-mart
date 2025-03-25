0134477ee2154f2f5641e47492485b59bcb00b1c
cunmann <Hoangmanh6889@gmail.com>
2025-03-25 17:52:57 +0700

    Add Cart API

--- src/main/kotlin/com/example/admp_api_flora_mart/controller/orderItem/OrderItemController.kt
+++ src/main/kotlin/com/example/admp_api_flora_mart/controller/orderItem/OrderItemController.kt
@@ -2,20 +2,23 @@ package com.example.admp_api_flora_mart.controller.orderItem
 
 import com.example.admp_api_flora_mart.controller.orderItem.request.AddToCartRequest
 import com.example.admp_api_flora_mart.service.OrderItemService
+import com.example.admp_api_flora_mart.service.OrderService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/v1/cart") // Đổi tên endpoint cho rõ ràng hơn
-class OrderItemController(private val orderItemService: OrderItemService) {
-
-    @PostMapping("/add")
-    fun addToCart(@RequestBody request: AddToCartRequest): ResponseEntity<Any> {
+@RequestMapping("/api/v1/orderItem") // Đổi tên endpoint cho rõ ràng hơn
+class OrderItemController(
+    private val orderItemService: OrderItemService,
+    private val orderService: OrderService,
+){
+    @PostMapping
+    fun addOrderItem(@RequestBody request: AddToCartRequest): ResponseEntity<Any> {
         return try {
-            val order = orderItemService.addToCart(request)
-            ResponseEntity.ok(order)
-        } catch (e: Exception) {
-            ResponseEntity.badRequest().body(mapOf("error" to e.message))
+            val newOrderItem = orderItemService.addToCart(request.orderItemDTO, request.cartDTO)
+            ResponseEntity.ok(newOrderItem)
+        } catch (ex: Exception){
+            ResponseEntity.badRequest().body(mapOf("error" to ex.message))
         }
     }
 }
